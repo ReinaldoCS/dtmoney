26ff0eb6c5ee91b4387dbcd362fe6025f5544e9e
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-08-04 13:00:41 -0300

    Instalando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257E6;
+
+  button {
+    background: #000;
+  }
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <Title>
+        Hello Word
+        <button>Salvar</button>
+      </Title>
     </div>
   );
 }
