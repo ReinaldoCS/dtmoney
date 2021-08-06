0b093b77cc1f1cce86c144cda3aed1d0f8518920
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-08-06 12:48:42 -0300

    Configurando cliente do Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransitionTable() {
   useEffect(() => {
-    fetch('http://localhost:3000/api/transitions')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    api.get('transitions')
+    .then(response => console.log(response.data))
   }, [])
 
   return (
