9099ebbb594a2787f456d0e688e7e745de830582
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-08-06 12:42:02 -0300

    Configurando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //todas chamadas a api vão esta a partir do .../api/
+    this.get('/transitions', () => {
+      return [
+        {
+          id: '1',
+          title: 'Transitions 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
